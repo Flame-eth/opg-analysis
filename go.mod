7e3919fc73f5ef2761347125a33754e5e5ed48d9
Flame-eth <theflamepen@gmail.com>
2024-11-22 14:06:14 +0100

    Add .gitignore files and update go.mod for dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Flame-eth/rss-aggregator
 
 go 1.23.3
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi v1.5.5
--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("Port is not set")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server running on ppprt %s", portString)
+	err = srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Error starting server")
+	}
 	
 }
